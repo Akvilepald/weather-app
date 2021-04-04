a8ec8d548f674acddc7fcc2a724733499bb75f59
Akvilepald <80216262+Akvilepald@users.noreply.github.com>
2021-04-04 20:07:55 +0100

    Added API

---
+++ src/app.js
@@ -0,0 +1,20 @@
+let apiKey = "175ad63a7fc75a67f734a3105255cb29";
+
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Edinburgh&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+
+function displayTemperature(response) {
+console.log(response);
+let temperatureElement = document.querySelector("#degrees");
+temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°C`
+let cityElement = document.querySelector("#city");
+cityElement.innerHTML = response.data.name;
+let descriptionElement = document.querySelector(".description");
+descriptionElement.innerHTML = response.data.weather[0].main;
+let humidityElement = document.querySelector(".humidity-percent");
+humidityElement.innerHTML = `${response.data.main.humidity}%`;
+let windElement = document.querySelector(".wind-speed");
+windElement.innerHTML = `${response.data.wind.speed} m/s`
+}
+
+axios.get(apiUrl).then(displayTemperature);
