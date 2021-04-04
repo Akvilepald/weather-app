875682cc914099c6f377d1615c7a6491ed38ad4f
Akvilepald <80216262+Akvilepald@users.noreply.github.com>
2021-04-04 20:33:30 +0100

    added current time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+let now = new Date();
+let weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = weekDay[now.getDay()];
+let monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let month = monthName[now.getMonth()];
+let fullDate = `${(now.getHours())}:${(now.getMinutes())}, ${day} ${(now.getDate())} ${month}`;
+console.log(fullDate);
+let dateElement = document.querySelector(".today-date");
+dateElement.innerHTML = `${fullDate}`;
+
 let apiKey = "175ad63a7fc75a67f734a3105255cb29";
 
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Edinburgh&appid=${apiKey}&units=metric`;
