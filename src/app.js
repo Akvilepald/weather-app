c993c81c955ddc464ed9ab0ee41e73000c530c52
Akvilepald <80216262+Akvilepald@users.noreply.github.com>
2021-04-08 00:13:18 +0100

    Added main weather icon API

--- src/app.js
+++ src/app.js
@@ -8,23 +8,27 @@ console.log(fullDate);
 let dateElement = document.querySelector(".today-date");
 dateElement.innerHTML = `${fullDate}`;
 
-let apiKey = "175ad63a7fc75a67f734a3105255cb29";
 
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Edinburgh&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
 
 function displayTemperature(response) {
 console.log(response);
 let temperatureElement = document.querySelector("#degrees");
-temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°C`
 let cityElement = document.querySelector("#city");
-cityElement.innerHTML = response.data.name;
 let descriptionElement = document.querySelector(".description");
-descriptionElement.innerHTML = response.data.weather[0].main;
 let humidityElement = document.querySelector(".humidity-percent");
-humidityElement.innerHTML = `${response.data.main.humidity}%`;
 let windElement = document.querySelector(".wind-speed");
-windElement.innerHTML = `${response.data.wind.speed} m/s`
+let mainIconElement = document.querySelector("#icon")
+
+temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°C`
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = `${response.data.main.humidity}%`;
+windElement.innerHTML = `${response.data.wind.speed} m/s`;
+mainIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
+
+let apiKey = "175ad63a7fc75a67f734a3105255cb29";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
